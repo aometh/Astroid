2aaa68525d6793aa55b65712fef60a1a040f30ee
aometh <edwin.orellana92@gmail.com>
2024-11-19 15:04:49 +1100

    added Shapes and FPS

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import *
 def main():
 	pygame.init()
+	gameTime = pygame.time.Clock()
+	dt = 0
+	framerate = 60
 
 	black = (0,0,0)
 
@@ -13,14 +16,13 @@ def main():
 				return
 		screen.fill(black)
 		pygame.display.flip()
-		
+		gameTime.tick(60)
 
 	
 
 	print("Starting asteroids!")
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
-
 
 if __name__== "__main__":
 	main()
